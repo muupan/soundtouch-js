ef3d6f55629a96898b12733fd90fb25ef2bb981c
Adrian Holovaty <adrian@holovaty.com>
2013-04-22 22:09:08 -0500

    Added 'window' as a parameter to the module-level anonymous function in st.js

--- src/js/st.js
+++ src/js/st.js
@@ -18,7 +18,7 @@
 * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
-(function() {
+(function(window) {
 
 /**
 * Giving this value for the sequence length sets automatic parameter value
@@ -904,4 +904,4 @@ window.soundtouch = {
     'SoundTouch': SoundTouch
 };
 
-})();
+})(window);
